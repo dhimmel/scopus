f46aa0da3b71b48188e882986fadf639740dcdc6
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-04-17 11:37:39 -0400

    Fix inconsistent tab/space indentation

--- download/download.sh
+++ download/download.sh
@@ -2,7 +2,7 @@
 for url in \
   'https://www.elsevier.com/__data/assets/excel_doc/0015/91122/title_list.xlsx' \
   'https://journalmetrics.scopus.com/downloads/metrics/CiteScore_Metrics_2011-2105_Download_19_Jan2017.xlsx' \
-	'https://raw.githubusercontent.com/dhimmel/delays/756ffebf309499a500ec1f83d68803c044ec8729/data/pubmed-journals.tsv';
+  'https://raw.githubusercontent.com/dhimmel/delays/756ffebf309499a500ec1f83d68803c044ec8729/data/pubmed-journals.tsv';
 do
-	wget --timestamping --no-verbose $url
+  wget --timestamping --no-verbose $url
 done
