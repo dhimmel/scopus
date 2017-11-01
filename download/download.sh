2d5dd1a148b027456af82e5cc3c8964f9a8f4d49
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-11-01 14:13:21 -0400

    Update Scopus downloads
    Analysis notebooks not yet updated with new Scopus inputs.

--- download/download.sh
+++ download/download.sh
@@ -1,7 +1,7 @@
 # Download input files. Execute from the `download` directory.
 for url in \
-  'https://www.elsevier.com/__data/assets/excel_doc/0015/91122/title_list.xlsx' \
-  'https://journalmetrics.scopus.com/downloads/metrics/CiteScore_Metrics_2011-2105_Download_19_Jan2017.xlsx' \
+  'https://www.elsevier.com/__data/assets/excel_doc/0015/91122/Scopus_Source_List.xlsx' \
+  'https://journalmetrics.scopus.com/downloads/metrics/CiteScore_Metrics_2011-2016_Download_21Jun2017.xlsx' \
   'https://raw.githubusercontent.com/dhimmel/delays/756ffebf309499a500ec1f83d68803c044ec8729/data/pubmed-journals.tsv';
 do
   wget --timestamping --no-verbose $url
